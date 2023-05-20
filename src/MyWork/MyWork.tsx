b2ebf7d91eff6e15de180d8d2cae48bc7903d8fe
Ваше ИмяgromVladgit commit -m new <client7289@gmail.com>
2023-05-20 11:17:18 +0300

    -

--- src/MyWork/MyWork.tsx
+++ src/MyWork/MyWork.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./MyWork.module.css";
 import socialNetwork from "../log/socialNetwork.png";
+import todolist from "../log/todolist.png";
 
 export function MyWork(): JSX.Element {
   return (
@@ -18,20 +19,19 @@ export function MyWork(): JSX.Element {
         </div>
 
         <div className={styles.project}>
-          <img src={socialNetwork} alt="Project 1" />
+          <img src={todolist} alt="Project 1" />
           <div className={styles.projectInfo}>
-            <p>Social network</p>
+            <p>Todolist</p>
           </div>
-          <a target="_blank" href="https://gromvlad.github.io/samurai_way_main">
+          <a target="_blank" href="https://gromvlad.github.io/todoList">
             <button className={styles.button}>view</button>
           </a>
         </div>
-
-        <div>
-          <a target="_blank" href="https://gromvlad.github.io/samurai_way_main">
-            <button className={styles.button}>view more</button>
-          </a>
-        </div>
+      </div>
+      <div className={styles.buttonMore}>
+        <a target="_blank" href="https://github.com/gromVlad">
+          <button className={styles.button}>view more</button>
+        </a>
       </div>
     </section>
   );
